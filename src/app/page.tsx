b10ebc5ca811591787bdc836a3e474bc644c271a
Hunter Wyatt <hunterwyatt@Hunters-Mac-mini.local>
2024-01-21 22:46:46 -0700

    fix deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-const BASE_URL = "https://api.collegefootballdata.com";
-const BEARER_TOKEN =
-  "8474sJ42lQEtgWGBYsru+i6Ltl9k05P+Ohf/ZccNOMc7xow9G409Cm2QxLbJxyaw";
-const TEAMS = "/teams/fbs";
-const ROSTER = "/roster";
+// const BASE_URL = "https://api.collegefootballdata.com";
+// const BEARER_TOKEN =
+//   "8474sJ42lQEtgWGBYsru+i6Ltl9k05P+Ohf/ZccNOMc7xow9G409Cm2QxLbJxyaw";
+// const TEAMS = "/teams/fbs";
+// const ROSTER = "/roster";
 
-export async function getData() {
-  const res = await fetch(`${BASE_URL}${TEAMS}`, {
-    headers: {
-      Authorization: `Bearer ${BEARER_TOKEN}`,
-    },
-  });
+// export async function getData() {
+//   const res = await fetch(`${BASE_URL}${TEAMS}`, {
+//     headers: {
+//       Authorization: `Bearer ${BEARER_TOKEN}`,
+//     },
+//   });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 export default async function Home() {
   return <div>HOME</div>;
