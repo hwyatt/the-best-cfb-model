ded05475cdc0d4a4850f9d998d4327ed86f17bd7
Hunter Wyatt <hunterwyatt@Hunters-Mac-mini.local>
2024-01-22 13:00:48 -0700

    portal page progress

--- src/app/portal/page.tsx
+++ src/app/portal/page.tsx
@@ -1,15 +1,52 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface YearOption {
+  label: string;
+  value: string;
+}
 
 export default function Portal() {
   const [data, setData] = useState([]);
-  const [year, setYear] = useState();
+  const [year, setYear] = useState("");
+  const [yearOpts, setYearOpts] = useState<YearOption[]>([]);
 
   const handleGetData = async () => {
-    const getPortal = await fetch(`/api/portal${year && `?year=${year}`}`);
-    const portalData = await getPortal.json();
-    setData(portalData);
+    if (year) {
+      const getPortal = await fetch(`/api/portal?year=${year}`);
+      const portalData = await getPortal.json();
+      setData(portalData);
+    }
   };
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+
+    // If we are not yet in August of the current year, set the current football year to the previous year
+    const currentYear =
+      currentMonth >= 7
+        ? currentDate.getFullYear()
+        : currentDate.getFullYear() - 1;
+
+    setYear(currentYear.toString());
+
+    const years = Array.from(
+      { length: 100 },
+      (_, index) => currentYear - index
+    );
+    const formattedYears = years.map((year) => ({
+      value: year.toString(),
+      label: year.toString(),
+    }));
+
+    setYearOpts(formattedYears);
+  }, []);
+
+  useEffect(() => {
+    // Use another useEffect to call handleGetData when the year changes
+    handleGetData();
+  }, [year]);
 
   return (
     <div className="flex flex-col gap-8">
