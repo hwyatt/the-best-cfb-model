de0956217cd3f46457ab976f8a7e08fb78dcda8e
Hunter Wyatt <hunterwyatt@Hunters-Mac-mini.local>
2024-01-24 23:36:12 -0700

    Werkin bracket

--- src/app/bracket/page.tsx
+++ src/app/bracket/page.tsx
@@ -195,7 +195,7 @@ export default function Bracket() {
   const TeamByIndex = ({ index, seed }: any) => {
     const team = playoffTeams[index - 1];
 
-    if (!team) {
+    if (!team || team.id === null) {
       // Index doesn't exist, you can return a placeholder or handle it as needed
       return <BlankTeam index={index} seed={seed} />;
     }
@@ -298,176 +298,262 @@ export default function Bracket() {
           playoffTeams={playoffTeams} // Pass playoffTeams prop
         />
       </div>
-      {/* {playoffTeams.length > 0 &&
-        playoffTeams.length <= 12 &&
-        playoffTeams[0].id !== null && (
-          <div className="flex flex-col gap-2 md:max-w-3xl">
-            <label className="font-semibold text-gray-600">
-              Reorder 12 Teams By Seed
-            </label>
 
-            <ul className="flex flex-col gap-2">
-              {playoffTeams.map((team, index) => {
-                if (team.id !== null) {
-                  return (
-                    <li key={team.id} className="flex gap-4 items-center">
-                      <p className="text-4xl font-bold text-gray-800">
-                        {index + 1}
-                      </p>
-                      <div
-                        className="flex items-center max-h-16 gap-2 bg-white border-2 border-gray-600 rounded p-4 w-full"
-                        style={{
-                          backgroundColor: `${
-                            team.color !== "#ffffff"
-                              ? team.color
-                              : team.alt_color
-                          }`,
-                        }}
-                      >
-                        <div className="bg-gray-200 rounded-full p-2">
-                          <img
-                            src={team.logos[0]}
-                            alt={`${team.school} Logo`}
-                            className="w-8 h-auto object-contain"
-                          />
-                        </div>
-                        <p className="text-white font-semibold">
-                          {team.school} {team.mascot}
-                        </p>
-                      </div>
-                    </li>
-                  );
+      <div className="hidden md:grid md:grid-cols-4 w-full gap-8">
+        <span className="font-semibold text-gray-600">ROUND ONE</span>
+        <span className="font-semibold text-gray-600">ROUND TWO</span>
+        <span className="font-semibold text-gray-600">ROUND THREE</span>
+        <span className="font-semibold text-gray-600">ROUND FOUR</span>
+      </div>
+      <div className="grid md:grid-cols-4 w-full gap-8">
+        <div className="flex flex-col gap-8 mt-12">
+          <div className="flex flex-col gap-4 relative">
+            {/* GAME 1 */}
+            <button onClick={() => setGame1Winner({ index: 8, seed: 8 })}>
+              <TeamByIndex index={8} seed={8} />
+            </button>
+            <button onClick={() => setGame1Winner({ index: 9, seed: 9 })}>
+              <TeamByIndex index={9} seed={9} />
+            </button>
+          </div>
+          <div className="flex flex-col gap-4">
+            {/* GAME 2 */}
+            <button onClick={() => setGame2Winner({ index: 5, seed: 5 })}>
+              <TeamByIndex index={5} seed={5} />
+            </button>
+            <button onClick={() => setGame2Winner({ index: 12, seed: 12 })}>
+              <TeamByIndex index={12} seed={12} />{" "}
+            </button>
+          </div>
+          <div className="flex flex-col gap-4">
+            {/* GAME 3 */}
+            <button onClick={() => setGame3Winner({ index: 6, seed: 6 })}>
+              <TeamByIndex index={6} seed={6} />
+            </button>
+            <button onClick={() => setGame3Winner({ index: 11, seed: 11 })}>
+              <TeamByIndex index={11} seed={11} />
+            </button>
+          </div>
+          <div className="flex flex-col gap-4">
+            {/* GAME 4 */}
+            <button onClick={() => setGame4Winner({ index: 7, seed: 7 })}>
+              <TeamByIndex index={7} seed={7} />
+            </button>
+            <button onClick={() => setGame4Winner({ index: 10, seed: 10 })}>
+              <TeamByIndex index={10} seed={10} />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-4">
+            {/* GAME 5 */}
+            <button onClick={() => setGame5Winner({ index: 1, seed: 1 })}>
+              <TeamByIndex index={1} seed={1} />
+            </button>
+            {game1Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame5Winner({
+                    index: game1Winner.index,
+                    seed: game1Winner.seed,
+                  })
                 }
-              })}
-            </ul>
+              >
+                <TeamByIndex
+                  index={game1Winner.index}
+                  seed={game1Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
           </div>
-        )} */}
-      {playoffTeams.length > 0 &&
-        playoffTeams.length <= 12 &&
-        playoffTeams[0].id !== null && (
-          <div className="grid md:grid-cols-4 w-full gap-8">
-            <div className="flex flex-col gap-8 mt-12">
-              <div className="flex flex-col gap-4">
-                <button onClick={() => setGame1Winner({ index: 8, seed: 8 })}>
-                  <TeamByIndex index={8} seed={8} />
-                </button>
-                <button onClick={() => setGame1Winner({ index: 9, seed: 9 })}>
-                  <TeamByIndex index={9} seed={9} />
-                </button>
-              </div>
-              <div className="flex flex-col gap-4">
-                <button onClick={() => setGame2Winner({ index: 5, seed: 5 })}>
-                  <TeamByIndex index={5} seed={5} />
-                </button>
-                <button onClick={() => setGame2Winner({ index: 12, seed: 12 })}>
-                  <TeamByIndex index={12} seed={12} />{" "}
-                </button>
-              </div>
-              <div className="flex flex-col gap-4">
-                <button onClick={() => setGame3Winner({ index: 6, seed: 6 })}>
-                  <TeamByIndex index={6} seed={6} />
-                </button>
-                <button onClick={() => setGame3Winner({ index: 11, seed: 11 })}>
-                  <TeamByIndex index={11} seed={11} />
-                </button>
-              </div>
-              <div className="flex flex-col gap-4">
-                <button onClick={() => setGame4Winner({ index: 7, seed: 7 })}>
-                  <TeamByIndex index={7} seed={7} />
-                </button>
-                <button onClick={() => setGame4Winner({ index: 10, seed: 10 })}>
-                  <TeamByIndex index={10} seed={10} />
-                </button>
-              </div>
-            </div>
-            <div className="flex flex-col gap-8">
-              <div className="flex flex-col gap-4">
-                <TeamByIndex index={1} seed={1} />
-                {game1Winner.index !== null ? (
-                  <TeamByIndex
-                    index={game1Winner.index}
-                    seed={game1Winner.seed}
-                  />
-                ) : (
-                  <BlankTeam />
-                )}
-              </div>
-              <div className="flex flex-col gap-4">
-                <TeamByIndex index={4} seed={4} />
-                {game2Winner.index !== null ? (
-                  <TeamByIndex
-                    index={game2Winner.index}
-                    seed={game2Winner.seed}
-                  />
-                ) : (
-                  <BlankTeam />
-                )}
-              </div>
-              <div className="flex flex-col gap-4">
-                <TeamByIndex index={3} seed={3} />
-                {game3Winner.index !== null ? (
-                  <TeamByIndex
-                    index={game3Winner.index}
-                    seed={game3Winner.seed}
-                  />
-                ) : (
-                  <BlankTeam />
-                )}
-              </div>
-              <div className="flex flex-col gap-4">
-                <TeamByIndex index={2} seed={2} />
-                {game4Winner.index !== null ? (
-                  <TeamByIndex
-                    index={game4Winner.index}
-                    seed={game4Winner.seed}
-                  />
-                ) : (
-                  <BlankTeam />
-                )}
-              </div>
-            </div>
-            <div className="flex flex-col gap-24 mt-8">
-              <div className="flex flex-col gap-32">
-                {game5Winner.index !== null ? (
-                  <div>Game 5 Winner</div>
-                ) : (
-                  <BlankTeam />
-                )}
-                {game6Winner.index !== null ? (
-                  <div>Game 6 Winner</div>
-                ) : (
-                  <BlankTeam />
-                )}
-              </div>
-              <div className="flex flex-col gap-32">
-                {game7Winner.index !== null ? (
-                  <div>Game 7 Winner</div>
-                ) : (
-                  <BlankTeam />
-                )}
-                {game8Winner.index !== null ? (
-                  <div>Game 8 Winner</div>
-                ) : (
-                  <BlankTeam />
-                )}
-              </div>
-            </div>
-            <div className="flex flex-col gap-8 mt-32">
-              <div className="flex flex-col gap-72">
-                {game9Winner.index !== null ? (
-                  <div>Game 9 Winner</div>
-                ) : (
-                  <BlankTeam />
-                )}
-                {game10Winner.index !== null ? (
-                  <div>Game 10 Winner</div>
-                ) : (
-                  <BlankTeam />
-                )}
-              </div>
-            </div>
+          <div className="flex flex-col gap-4">
+            {/* GAME 6 */}
+            <button onClick={() => setGame6Winner({ index: 4, seed: 4 })}>
+              <TeamByIndex index={4} seed={4} />
+            </button>
+            {game2Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame6Winner({
+                    index: game2Winner.index,
+                    seed: game2Winner.seed,
+                  })
+                }
+              >
+                <TeamByIndex
+                  index={game2Winner.index}
+                  seed={game2Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
           </div>
-        )}
+          <div className="flex flex-col gap-4">
+            {/* GAME 7 */}
+            <button onClick={() => setGame7Winner({ index: 3, seed: 3 })}>
+              <TeamByIndex index={3} seed={3} />
+            </button>
+            {game3Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame7Winner({
+                    index: game3Winner.index,
+                    seed: game3Winner.seed,
+                  })
+                }
+              >
+                <TeamByIndex
+                  index={game3Winner.index}
+                  seed={game3Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
+          </div>
+          <div className="flex flex-col gap-4">
+            {/* GAME 8 */}
+            <button onClick={() => setGame8Winner({ index: 2, seed: 2 })}>
+              <TeamByIndex index={2} seed={2} />
+            </button>
+            {game4Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame8Winner({
+                    index: game4Winner.index,
+                    seed: game4Winner.seed,
+                  })
+                }
+              >
+                <TeamByIndex
+                  index={game4Winner.index}
+                  seed={game4Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col gap-24 mt-8">
+          <div className="flex flex-col gap-32">
+            {/* GAME 9 */}
+            {game5Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame9Winner({
+                    index: game5Winner.index,
+                    seed: game5Winner.seed,
+                  })
+                }
+              >
+                <TeamByIndex
+                  index={game5Winner.index}
+                  seed={game5Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
+            {game6Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame9Winner({
+                    index: game6Winner.index,
+                    seed: game6Winner.seed,
+                  })
+                }
+              >
+                <TeamByIndex
+                  index={game6Winner.index}
+                  seed={game6Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
+          </div>
+          <div className="flex flex-col gap-32">
+            {/* GAME 10 */}
+            {game7Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame10Winner({
+                    index: game7Winner.index,
+                    seed: game7Winner.seed,
+                  })
+                }
+              >
+                <TeamByIndex
+                  index={game7Winner.index}
+                  seed={game7Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
+            {game8Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame10Winner({
+                    index: game8Winner.index,
+                    seed: game8Winner.seed,
+                  })
+                }
+              >
+                <TeamByIndex
+                  index={game8Winner.index}
+                  seed={game8Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col gap-8 mt-32">
+          <div className="flex flex-col gap-72">
+            {/* GAME 11 */}
+            {game9Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame11Winner({
+                    index: game9Winner.index,
+                    seed: game9Winner.seed,
+                  })
+                }
+              >
+                <TeamByIndex
+                  index={game9Winner.index}
+                  seed={game9Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
+            {game10Winner.index !== null ? (
+              <button
+                onClick={() =>
+                  setGame11Winner({
+                    index: game10Winner.index,
+                    seed: game10Winner.seed,
+                  })
+                }
+              >
+                <TeamByIndex
+                  index={game10Winner.index}
+                  seed={game10Winner.seed}
+                />
+              </button>
+            ) : (
+              <BlankTeam />
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
