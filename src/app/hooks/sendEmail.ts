494379654ea3020319d0473de3d0738d296bca25
Hunter Wyatt <hunterwyatt@Hunters-Mac-mini.local>
2024-02-07 14:34:56 -0700

    werk

--- src/app/hooks/sendEmail.ts
+++ src/app/hooks/sendEmail.ts
@@ -10,8 +10,8 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendEmail: any = async (name: any, email: any, message: any) => {
   const msg = {
-    to: "huntercwyatt@gmail.com", // Change to your email address
-    from: "huntercwyatt@gmail.com", // Change to your verified sender
+    to: "hunter@saturdaystats.com", // Change to your email address
+    from: "hunter@saturdaystats.com", // Change to your verified sender
     subject: "New Message from Contact Form",
     text: `${name} (${email}) says: ${message}`,
     html: `<p>${name} (${email}) says:</p><p>${message}</p>`,
