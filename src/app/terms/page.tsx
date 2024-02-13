ce79a122a429a372d14c8ceccb3c46e2bc3fb323
Hunter Wyatt <hunterwyatt@Hunters-Mac-mini.local>
2024-02-12 22:42:04 -0700

    terms

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,84 @@
+const Terms = () => {
+  return (
+    <div className="">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Terms of Use</h1>
+      <p className="mb-6">
+        These Terms of Use outline the rules and regulations for the use of our
+        website.
+      </p>
+      <h2 className="text-xl font-semibold text-gray-800 mb-2">
+        License to Use Website
+      </h2>
+      <p className="mb-6">
+        Unless otherwise stated, SaturdayStats.com and/or its licensors own the
+        intellectual property rights for all material on SaturdayStats.com. All
+        intellectual property rights are reserved. You may view and/or print
+        pages from SaturdayStats.com for your own personal use, subject to
+        restrictions set in these terms and conditions.
+      </p>
+      <h2 className="text-xl font-semibold text-gray-800 mb-2">
+        Acceptable Use
+      </h2>
+      <p className="mb-6">
+        You must not use this website in any way that causes, or may cause,
+        damage to the website or impairment of the availability or accessibility
+        of SaturdayStats.com, or in any way which is unlawful, illegal,
+        fraudulent, or harmful, or in connection with any unlawful, illegal,
+        fraudulent, or harmful purpose or activity.
+      </p>
+      <h2 className="text-xl font-semibold text-gray-800 mb-2">Disclaimer</h2>
+      <p className="mb-6">
+        SaturdayStats.com is for entertainment purposes only. While we strive to
+        provide accurate and up-to-date information, we make no representations
+        or warranties of any kind, express or implied, about the completeness,
+        accuracy, reliability, suitability, or availability with respect to the
+        website or the information, products, services, or related graphics
+        contained on the website for any purpose. Any reliance you place on such
+        information is therefore strictly at your own risk.
+      </p>
+      <p className="mb-6">
+        Some of the data provided on SaturdayStats.com is sourced from{" "}
+        <a
+          href="https://collegefootballdata.com/"
+          className="text-blue-600 hover:underline"
+        >
+          collegefootballdata.com
+        </a>
+        . We cannot guarantee the accuracy or completeness of this data and
+        shall not be held liable for any inaccuracies.
+      </p>
+      <h2 className="text-xl font-semibold text-gray-800 mb-2">
+        Limitation of Liability
+      </h2>
+      <p className="mb-6">
+        In no event shall SaturdayStats.com, nor any of its officers, directors,
+        and employees, be liable to you for anything arising out of or in any
+        way connected with your use of this website, whether such liability is
+        under contract, tort, or otherwise, and SaturdayStats.com, including its
+        officers, directors, and employees shall not be liable for any indirect,
+        consequential, or special liability arising out of or in any way related
+        to your use of this website.
+      </p>
+      <h2 className="text-xl font-semibold text-gray-800 mb-2">
+        Variation of Terms
+      </h2>
+      <p className="mb-6">
+        SaturdayStats.com is permitted to revise these Terms of Use at any time
+        as it sees fit, and by using this website you are expected to review
+        these Terms of Use on a regular basis.
+      </p>
+      <h2 className="text-xl font-semibold text-gray-800 mb-2">
+        Governing Law {`&`} Jurisdiction
+      </h2>
+      <p className="mb-6">
+        These Terms of Use will be governed by and construed in accordance with
+        the laws of the United States, and you submit to the non-exclusive
+        jurisdiction of the state and federal courts located in the United
+        States for the resolution of any disputes.
+      </p>
+      <p className="text-sm text-gray-600">Last Updated: February 12, 2024</p>
+    </div>
+  );
+};
+
+export default Terms;
